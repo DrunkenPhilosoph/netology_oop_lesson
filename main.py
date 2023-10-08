e51d9c4c0ab8fcbabfa89ca53891430487b9a06f
Eugene <pepiko@yandex.ru>
2023-10-08 22:37:46 +0300

    change main.py

--- main.py
+++ main.py
@@ -7,12 +7,34 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def add_raring_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached:
+            if self.finished_courses in lecturer.grade:
+                if grade <= 10 and grade > 0:
+                    lecturer.grade[course] = []
+                    lecturer.grade[course].append(grade)
+                else:
+                    return 'Значение оценки должно быть от 0 до 10'
+            else:
+                return 'Лектор не ведет этот курс'
+
+
+
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname, courses_attached):
+        super().__init__(name, surname, courses_attached)
+        self.grade = {}
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname, courses_attached):
+        super().__init__(name, surname, courses_attached)
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -29,6 +51,9 @@ best_student.courses_in_progress += ['Python']
 
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
+
+cool_lecturer = Lecturer('John', 'Smith', 'Python')
+
 
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
