7d2996dd363ed7dba08f5f24c2487ab3f4fa7a94
DrunkenPhilosoph <130548981+DrunkenPhilosoph@users.noreply.github.com>
2023-10-11 16:16:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class Student:
             tmp += self.grades[i]
         avg = sum(tmp) / len(tmp)
         return avg
+
     def add_raring_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer):
             if course in lecturer.courses_attached:
@@ -32,8 +33,13 @@ class Student:
         else:
             return 'Ошибка'
 
-
-
+    def __eq__(self, other):
+        if self.avg_rate() > other.avg_rate():
+            return f"Имя: {self.name} балл: {self.avg_rate()}"
+        elif self.avg_rate() < other.avg_rate():
+            return f"Имя: {other.name} балл: {other.avg_rate()}"
+        else:
+            return f"Баллы одинаковые у {self.name} и {other.name}"
 
 class Mentor:
     def __init__(self, name, surname):
@@ -45,11 +51,19 @@ class Mentor:
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
-        super().__init__( name, surname)
+        super().__init__(name, surname)
         self.courses_attached = []
         self.grades = {}
     def __str__(self):
         return f"Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции {self.avg_rate()}"
+
+    def __eq__(self, other):
+        if self.avg_rate() > other.avg_rate():
+            return f"Студент: {self.name} {self.surname}.\nСредний балл: {self.avg_rate()}"
+        elif self.avg_rate() < other.avg_rate():
+            return f"Студент: {other.name} {other.surname}.\nСредний балл: {other.avg_rate()}"
+        else:
+            return f"Баллы одинаковые у {self.name} {self.surname} и {other.name} {other.surname}.\nСреднее количество баллов: {other.avg_rate()}"
     def avg_rate(self):
         tmp = []
         for i in self.grades:
@@ -75,33 +89,65 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student = Student('Ruoy', 'Eman', 'Male')
+best_student_two = Student('Erman', 'Chekovskiy', 'Male')
 best_student.courses_in_progress += ['Python']
+best_student_two.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['Git']
+best_student_two.courses_in_progress += ['Git']
+best_student.finished_courses += ['Git']
+best_student_two.finished_courses += ['Git']
 
-cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer = Reviewer('Igor', 'Gatsky')
 cool_reviewer.courses_attached += ['Python']
+cool_reviewer.courses_attached += ['Git']
 
 cool_lecturer = Lecturer('John', 'Smith')
+cool_lecturer_two = Lecturer('Stein', 'Silver')
+cool_lecturer_two.courses_attached += ['Python']
+cool_lecturer_two.courses_attached += ['JS']
+cool_lecturer_two.courses_attached += ['Git']
 cool_lecturer.courses_attached += ['Python']
 cool_lecturer.courses_attached += ['JS']
+cool_lecturer.courses_attached += ['Git']
 
 best_student.add_raring_lecturer(cool_lecturer, 'Python', 5)
+best_student_two.add_raring_lecturer(cool_lecturer, 'Python', 2)
 best_student.add_raring_lecturer(cool_lecturer, 'Python', 10)
+best_student_two.add_raring_lecturer(cool_lecturer, 'Python', 9)
 best_student.add_raring_lecturer(cool_lecturer, 'Python', 6)
+best_student_two.add_raring_lecturer(cool_lecturer, 'Python', 1)
 best_student.add_raring_lecturer(cool_lecturer, 'JS', 6)
+best_student_two.add_raring_lecturer(cool_lecturer, 'JS', 2)
 best_student.add_raring_lecturer(cool_lecturer, 'JS', 6)
+best_student_two.add_raring_lecturer(cool_lecturer, 'JS', 10)
 
+best_student.add_raring_lecturer(cool_lecturer_two, 'Python', 5)
+best_student_two.add_raring_lecturer(cool_lecturer_two, 'Python', 10)
+best_student.add_raring_lecturer(cool_lecturer_two, 'Python', 4)
+best_student_two.add_raring_lecturer(cool_lecturer_two, 'Python', 5)
+best_student.add_raring_lecturer(cool_lecturer_two, 'Python', 6)
+best_student_two.add_raring_lecturer(cool_lecturer_two, 'Python', 1)
+best_student.add_raring_lecturer(cool_lecturer_two, 'JS', 1)
+best_student_two.add_raring_lecturer(cool_lecturer_two, 'JS', 6)
 
 cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Git', 5)
-cool_reviewer.rate_hw(best_student, 'Git', 5)
-cool_reviewer.rate_hw(best_student, 'Git', 10)
+cool_reviewer.rate_hw(best_student_two, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 3)
+cool_reviewer.rate_hw(best_student_two, 'Python', 4)
+cool_reviewer.rate_hw(best_student, 'Python', 1)
+cool_reviewer.rate_hw(best_student_two, 'Python', 2)
+cool_reviewer.rate_hw(best_student, 'Git', 3)
+cool_reviewer.rate_hw(best_student_two, 'Git', 7)
+cool_reviewer.rate_hw(best_student, 'Git', 6)
+cool_reviewer.rate_hw(best_student_two, 'Git', 9)
+cool_reviewer.rate_hw(best_student, 'Git', 1)
+cool_reviewer.rate_hw(best_student_two, 'Git', 1)
 
 # print(best_student.grades)
-# print(cool_lecturer)
-print(best_student.grades)
+print(cool_lecturer == cool_lecturer_two)
+# print(best_student.grades)
 # print(cool_reviewer)
-# print(cool_reviewer)
+# print(cool_lecturer.grades)
+
+# print(best_student_two)
